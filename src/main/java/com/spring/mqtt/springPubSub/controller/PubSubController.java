2747493cd41be5c5112d411b0a33e0fec4f5a054
navdeep <navdeep@site1.localhost>
2017-09-20 11:30:59 +0530

    Intial commit

---
+++ src/main/java/com/spring/mqtt/springPubSub/controller/PubSubController.java
@@ -0,0 +1,63 @@
+package com.spring.mqtt.springPubSub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PubSubController {
+
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("index");
+		return model;
+
+	}
+	
+	@RequestMapping(value = { "/buttons" }, method = RequestMethod.GET)
+	public ModelAndView ledButtons() {
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("buttons");
+		return model;
+
+	}
+	
+	@RequestMapping(value = { "/ledOn" }, method = RequestMethod.POST)
+	public String ledOn() {
+
+		return "redirect:buttons";
+
+	}
+	
+	@RequestMapping(value = { "/ledOff" }, method = RequestMethod.POST)
+	public String ledOff() {
+
+		return "redirect:buttons";
+
+	}
+	
+/*	@RequestMapping(value = { "/ledSwitch" }, method = RequestMethod.POST)
+	public void ledController() {
+		if()
+
+	}*/
+
+	/*
+	 * @RequestMapping(value = "/login", method = RequestMethod.GET) public
+	 * ModelAndView adminPage() {
+	 * 
+	 * ModelAndView model = new ModelAndView(); model.addObject("title",
+	 * "Spring Security Custom Login Form"); model.addObject("message",
+	 * "This is protected page!"); model.setViewName("login");
+	 * 
+	 * return model;
+	 * 
+	 * }
+	 */
+
+}
